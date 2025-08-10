1aee3bd2646dfe09b1992e3bc46e54283e7bf2b7
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-08-10 19:53:51 -0300

    Adicionando resolução do desafio com adição do método ToString

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,26 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Iphone Instalando aplicativo {nomeApp}");
+        }
+        
+        public override string ToString()
+        {
+            string texto = $"Número: {this.Numero}" + "\n" +
+                           $"Modelo: {this.Modelo}" + "\n" +
+                           $"IMEI: {this.IMEI}" + "\n" +
+                           $"Memória: {this.Memoria}";
+            return texto;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿using DesafioPOO.Models;
+﻿using System.Data;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("$$$ TESTES COM NOKIA $$$");
+Nokia nokia = new Nokia(numero: "62995956321", modelo: "Nokia C-5", imei: "1236548987", 32);
+Console.WriteLine(nokia);
+
+Console.WriteLine();
+
+nokia.Ligar();
+Console.WriteLine();
+
+nokia.ReceberLigacao();
+Console.WriteLine();
+
+nokia.InstalarAplicativo("Chrome");
+
+
+// testes iphone
+Console.WriteLine("\n$$$ TESTES COM IPHONE $$$");
+Iphone iphone = new Iphone(numero: "62925584725", modelo: "Iphone 11", imei: "994455228899", 128);
+Console.WriteLine(iphone);
+
+Console.WriteLine();
+
+iphone.Ligar();
+Console.WriteLine();
+
+iphone.ReceberLigacao();
+Console.WriteLine();
+
+iphone.InstalarAplicativo("Chrome");
